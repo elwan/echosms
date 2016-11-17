3b8d8b09ca78c45b575d57115c06139358628aac
elwan <elwan7@gmail.com>
2016-11-17 13:49:57 +0000

    plateforme sms

--- accounts/forms.py
+++ accounts/forms.py
@@ -15,7 +15,7 @@ class UserCreation(UserCreationForm):
     username = forms.CharField(label="", max_length=30, widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'login', 'placeholder': 'Login', 'required': 'True', 'autofocus': 'True'}))
     first_name = forms.CharField(label="", max_length=30, widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'Prenom', 'placeholder': 'Prénom', 'required': 'True', 'autofocus': 'True'}))
     last_name = forms.CharField(label="", max_length=30, widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'Nom', 'placeholder': 'Nom', 'required': 'True', 'autofocus': 'True'}))
-    numero_telephone = forms.CharField(label="", max_length=9, widget=IntlTelInputWidget(attrs={'required': 'True'}))
+    numero_telephone = forms.CharField(label="", max_length=15, widget=IntlTelInputWidget(attrs={'required': 'True'}))
     email = forms.CharField(label="", max_length=30, widget=forms.EmailInput(attrs={'class': 'form-control', 'name': 'Email', 'placeholder': 'Email', 'required': 'True', 'autofocus': 'True'}))
     password1 = forms.CharField(label="", max_length=30, widget=forms.PasswordInput(attrs={'class': 'form-control', 'name': 'password', 'placeholder': 'password', 'required': 'True', 'autofocus': 'True'}))
     password2 = forms.CharField(label="", max_length=30, widget=forms.PasswordInput(attrs={'class': 'form-control', 'name': 'password_validation', 'placeholder': 'password validation', 'required': 'True', 'autofocus': 'True'}))
